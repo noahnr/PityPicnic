43095bd7f6e028a48fba796f4c6a2ba564e3d1aa
noahnr <noahsoukupraesch@gmail.com>
2020-11-29 12:27:50 -0600

    new reopo made foir herou

---
+++ client/src/components/FriendCard/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./style.css";
+
+function FriendCard(props) {
+  return (
+    <div className="card">
+      <div className="img-container">
+        <img alt={props.name} src={props.image} />
+      </div>
+      <div className="content">
+        <ul>
+          <li>
+            <strong>Name:</strong> {props.name}
+          </li>
+          <li>
+            <strong>Price:</strong> {props.price}
+          </li>
+          <li>
+
+          <a href="https://www.etsy.com/shop/PityPicnic?ele=shop_open" class="button">Click Here</a>          
+          </li>
+         
+        </ul>
+      </div>
+      {/* <span onClick={() => props.removeFriend(props.id)} className="remove">
+        𝘅
+      </span> */}
+    </div>
+  );
+}
+
+export default FriendCard;
